d4706b62d2fdd66742c638d220a76fcae7a89ef2
Prasoon Goyal <pgoyal@cs.utexas.edu>
2020-02-11 23:09:30 -0600

    Minor changes.

--- src/supervised/data.py
+++ src/supervised/data.py
@@ -13,131 +13,139 @@ DATA_DIR = '../../data/'
 FRAMES_DIR = '../../data/frames/'
 ENVS_DIR = '../../data/envs/'
 
-class PadBatch:
-  def __init__(self):
-    pass
+TRAIN_VIDEOS = list(range(80))
+VALID_VIDEOS = list(range(80, 100))
+TEST_VIDEOS = [50, 92, 95]
 
-  def __call__(self, batch):
-    traj_right_batch, traj_left_batch, traj_center_batch, \
-    lang_batch, lang_enc_batch, traj_len_batch, lang_len_batch, labels_batch, obj_batch, \
-        env_batch, weight_batch = zip(*batch)
-    traj_right_batch = pad_sequence(traj_right_batch, batch_first=True)
-    traj_left_batch = pad_sequence(traj_left_batch, batch_first=True)
-    traj_center_batch = pad_sequence(traj_center_batch, batch_first=True)
-    lang_enc_batch = pad_sequence(lang_enc_batch, batch_first=True)
-    lang_enc_batch = torch.from_numpy(np.array(lang_enc_batch))
-    traj_len_batch = torch.Tensor(traj_len_batch)
-    lang_len_batch = torch.Tensor(lang_len_batch)
-    weight_batch = torch.Tensor(weight_batch)
-    labels_batch = torch.Tensor(labels_batch)
-    return traj_right_batch, traj_left_batch, traj_center_batch, \
-        lang_batch, lang_enc_batch, traj_len_batch, lang_len_batch, \
-        labels_batch, obj_batch, env_batch, weight_batch
+class PadBatch:
+    def __init__(self):
+        pass
+
+    def __call__(self, batch):
+        traj_right_batch, traj_left_batch, traj_center_batch, \
+        lang_batch, lang_enc_batch, traj_len_batch, lang_len_batch, labels_batch, obj_batch, \
+                env_batch, weight_batch = zip(*batch)
+        traj_right_batch = pad_sequence(traj_right_batch, batch_first=True)
+        traj_left_batch = pad_sequence(traj_left_batch, batch_first=True)
+        traj_center_batch = pad_sequence(traj_center_batch, batch_first=True)
+        lang_enc_batch = pad_sequence(lang_enc_batch, batch_first=True)
+        lang_enc_batch = torch.from_numpy(np.array(lang_enc_batch))
+        traj_len_batch = torch.Tensor(traj_len_batch)
+        lang_len_batch = torch.Tensor(lang_len_batch)
+        weight_batch = torch.Tensor(weight_batch)
+        labels_batch = torch.Tensor(labels_batch)
+        return traj_right_batch, traj_left_batch, traj_center_batch, \
+                lang_batch, lang_enc_batch, traj_len_batch, lang_len_batch, \
+                labels_batch, obj_batch, env_batch, weight_batch
 
 class Data(Dataset):
-  def __init__(self, mode, sampling, prefix, repeat=1):
-    self.sampling = sampling
-    self.prefix = prefix
-    self.vocab = pickle.load(open('{}/vocab_train.pkl'.format(DATA_DIR), 'rb'))
-    self.descriptions = self.load_descriptions(mode)
-    if mode == 'train':
-      self.video_ids = list(range(80))
-      self.video_ids.remove(50)
-    elif mode == 'valid':
-      self.video_ids = list(range(80, 100))
-      self.video_ids.remove(92)
-      self.video_ids.remove(95)
-    else:
-      raise NotImplementedError('Invalid mode!')
-    self.N_OBJ = 13
-    self.N_ENV = len(self.video_ids)
-    self.thresh = 0.
-    self.len_frac = 0.
-    self.repeat = repeat
+    def __init__(self, mode, sampling, prefix, repeat=1):
+        self.sampling = sampling
+        self.prefix = prefix
+        self.vocab = pickle.load(open('{}/vocab_train.pkl'.format(DATA_DIR), 'rb'))
+        self.descriptions = self.load_descriptions(mode)
+        self.video_ids = self.get_video_ids(mode)
+        self.N_OBJ = 13
+        self.N_ENV = len(self.video_ids)
+        self.repeat = repeat
 
-  def update_len_frac(self):
-    self.len_frac = max(0, self.len_frac - 0.1)
+    def get_video_ids(self, mode):
+        if mode == 'train':
+            video_ids = TRAIN_VIDEOS
+        elif mode == 'valid':
+            video_ids = VALID_VIDEOS
+        else:
+            raise NotImplementedError('Invalid mode!')
+        for vid in TEST_VIDEOS:
+            try:
+                video_ids.remove(vid)
+            except ValueError:
+                pass
+        return video_ids
 
-  def __len__(self):
-    return 2 * self.N_OBJ * self.N_ENV * self.repeat
+    def __len__(self):
+        return 2 * self.N_OBJ * self.N_ENV * self.repeat
 
-  def set_thresh(self, thresh):
-    self.thresh = thresh
+    def encode_description(self, descr):
+        result = []
+        for w in descr.split():
+                try:
+                        t = self.vocab.index(w)
+                except ValueError:
+                        t = self.vocab.index('<unk>')
+                result.append(t)
+        return torch.Tensor(result)
 
-  def encode_description(self, descr):
-    result = []
-    for w in descr.split():
-        try:
-            t = self.vocab.index(w)
-        except ValueError:
-            t = self.vocab.index('<unk>')
-        result.append(t)
-    return torch.Tensor(result)
+    def load_descriptions(self, mode):
+        descriptions = pickle.load(open('{}/{}_descr.pkl'.format(DATA_DIR, mode), 'rb'))
+        result = {}
+        for i in descriptions.keys():
+                descr_list = descriptions[i]
+                result[i] = [(d, self.encode_description(d)) for d in descr_list]
+        return result
 
-  def load_descriptions(self, mode):
-    descriptions = pickle.load(open('{}/{}_descr.pkl'.format(DATA_DIR, mode), 'rb'))
-    result = {}
-    for i in descriptions.keys():
-        descr_list = descriptions[i]
-        result[i] = [(d, self.encode_description(d)) for d in descr_list]
-    return result
+    def load_env_objects(self, obj, env):
+        result = []
+        with open('{}/obj{}-env{}.txt'.format(ENVS_DIR, obj, env)) as f:
+            for line in f.readlines():
+                line = line.replace('(', '').replace(',', '').replace(')', '')
+                parts = line.split()
+                x = eval(parts[0])
+                y = eval(parts[1])
+                obj = eval(parts[2])
+                result.append(obj)
+        return result
 
-  def load_env_objects(self, obj, env):
-    result = []
-    with open('{}/obj{}-env{}.txt'.format(ENVS_DIR, obj, env)) as f:
-      for line in f.readlines():
-        line = line.replace('(', '').replace(',', '').replace(')', '')
-        parts = line.split()
-        x = eval(parts[0])
-        y = eval(parts[1])
-        obj = eval(parts[2])
-        result.append(obj)
-    return result
+    def load_frames(self, obj, env):
+        frames_r = torch.from_numpy(torch.load(open('{}/obj{}-env{}-right-50x50.pt'.format(FRAMES_DIR, obj, env), 'rb')))
+        frames_l = torch.from_numpy(torch.load(open('{}/obj{}-env{}-left-50x50.pt'.format(FRAMES_DIR, obj, env), 'rb')))
+        frames_c = torch.from_numpy(torch.load(open('{}/obj{}-env{}-center-50x50.pt'.format(FRAMES_DIR, obj, env), 'rb')))
+        n_frames_total = len(frames_r)
 
-  def __getitem__(self, index):
-    if index >= len(self) // 2:
-      label = 0
-      index -= len(self) // 2
-    else:
-      label = 1
+        n_frames = np.random.randint(1, (n_frames_total+1))
+        weight = (n_frames / n_frames_total)
+        frames_r = frames_r[:n_frames]
+        frames_l = frames_l[:n_frames]
+        frames_c = frames_c[:n_frames]
 
-    obj = index // (self.repeat * self.N_ENV)
-    env = self.video_ids[index % self.N_ENV]
-    env_objects = self.load_env_objects(obj, env)
+        while True:
+            selected = np.random.random(n_frames) > 0.9
+            if np.sum(selected) > 0:
+                break
+        frames_r = frames_r[selected]
+        frames_l = frames_l[selected]
+        frames_c = frames_c[selected]
+            
+        return frames_r, frames_l, frames_c, n_frames_total, weight
 
-    frames_right = torch.from_numpy(torch.load(open('{}/obj{}-env{}-right-50x50.pt'.format(FRAMES_DIR, obj, env), 'rb')))
-    frames_left = torch.from_numpy(torch.load(open('{}/obj{}-env{}-left-50x50.pt'.format(FRAMES_DIR, obj, env), 'rb')))
-    frames_center = torch.from_numpy(torch.load(open('{}/obj{}-env{}-center-50x50.pt'.format(FRAMES_DIR, obj, env), 'rb')))
+    def get_descr(self, obj, label, env_objects):
+        if label == 1:
+            t = np.random.randint(0, len(self.descriptions[obj]))
+            descr, descr_enc = self.descriptions[obj][t]
+        else:
+            # select an alternate language
+            tt = np.random.random()
+            alt_obj = env_objects[1:]
+            if len(alt_obj) == 0:
+                alt_obj = list(range(0,obj)) + list(range(obj+1,self.N_OBJ))
+            obj_ = np.random.choice(alt_obj)
+            t = np.random.randint(0, len(self.descriptions[obj_])-1)
+            descr, descr_enc = self.descriptions[obj_][t]
+        return descr, descr_enc
 
-    lower = max(1, self.len_frac * (len(frames_right)+1) - 1)
-    t = np.random.randint(lower, (len(frames_right)+1))
-    weight = (t / len(frames_right))**1
-    frames_right = frames_right[:t]
-    frames_left = frames_left[:t]
-    frames_center = frames_center[:t]
+    def __getitem__(self, index):
+        if index >= len(self) // 2:
+            label = 0
+            index -= len(self) // 2
+        else:
+            label = 1
 
-    while True:
-      selected = np.random.random(len(frames_right)) > 0.9
-      if np.sum(selected) > 0:
-        break
-    frames_right = frames_right[selected]
-    frames_left = frames_left[selected]
-    frames_center = frames_center[selected]
-      
-    if label == 1:
-      t = np.random.randint(0, len(self.descriptions[obj]))
-      descr, descr_enc = self.descriptions[obj][t]
-    else:
-      tt = np.random.random()
-      # alternate lang
-      alt_obj = env_objects[1:]
-      # alt_obj = list(filter(lambda xyo: abs(env_objects[0][0] - xyo[0]) >= 2, env_objects))
-      # alt_obj = list(map(lambda xyo: xyo[-1], alt_obj))
-      if len(alt_obj) == 0:
-        alt_obj = list(range(0,obj)) + list(range(obj+1,self.N_OBJ))
-      obj_ = np.random.choice(alt_obj)
-      t = np.random.randint(0, len(self.descriptions[obj_])-1)
-      descr, descr_enc = self.descriptions[obj_][t]
-    label = 2*label - 1
-    return frames_right, frames_left, frames_center, descr, descr_enc, len(frames_right), len(descr_enc), label, obj, env, weight
+        obj = index // (self.repeat * self.N_ENV)
+        env = self.video_ids[index % self.N_ENV]
+        env_objects = self.load_env_objects(obj, env)
+
+        frames_r, frames_l, frames_c, n_frames_total, weight = self.load_frames(obj, env)
+        descr, descr_enc = self.get_descr(obj, label, env_objects)
+        label = 2*label - 1
+        return frames_r, frames_l, frames_c, descr, descr_enc, len(frames_r), len(descr_enc), label, obj, env, weight
 
--- src/supervised/model.py
+++ src/supervised/model.py
@@ -34,7 +34,8 @@ def lstm_helper(sequences, lengths, lstm):
     max_len = sequences.shape[1]
     max_len_true = output_nonzero.shape[1]
     output = torch.zeros(len(sequences), max_len, output_nonzero.shape[-1])
-    output_final = torch.zeros(len(sequences), max_len, output_nonzero.shape[-1])
+    output_final = torch.zeros(
+        len(sequences), max_len, output_nonzero.shape[-1])
     output[:nonzero, :max_len_true, :] = output_nonzero
     hidden = torch.zeros(len(sequences), hidden_nonzero.shape[-1])
     hidden_final = torch.zeros(len(sequences), hidden_nonzero.shape[-1])
@@ -74,12 +75,16 @@ class Model(nn.Module):
         super().__init__()
         self.args = args
 
-        self.img_enc_right = ImgEnc(args)
-        self.img_enc_left = ImgEnc(args)
-        self.img_enc_center = ImgEnc(args)
+        self.img_enc_r = ImgEnc(args)
+        self.img_enc_l = ImgEnc(args)
+        self.img_enc_c = ImgEnc(args)
 
         # trajectory encoder
-        self.traj_encoder = nn.LSTM(3 * args.img_enc_size, args.img_enc_size, batch_first=True, num_layers=args.num_layers)
+        self.traj_encoder = nn.LSTM(
+            3 * args.img_enc_size, 
+            args.img_enc_size, 
+            batch_first=True, 
+            num_layers=args.num_layers)
 
         # language encoder
         self.embedding = nn.Embedding(VOCAB_SIZE, args.lang_enc_size)
@@ -90,15 +95,15 @@ class Model(nn.Module):
         self.linear2 = nn.Linear(args.classifier_size, 1)
 
 
-    def forward(self, traj_right, traj_left, traj_center, lang, traj_len, lang_len):
-        traj_right_enc = self.img_enc_right(traj_right.view(-1, *traj_right.shape[-3:]))
-        traj_right_enc = traj_right_enc.view(*traj_right.shape[:2], -1)
-        traj_left_enc = self.img_enc_left(traj_left.view(-1, *traj_left.shape[-3:]))
-        traj_left_enc = traj_left_enc.view(*traj_left.shape[:2], -1)
-        traj_center_enc = self.img_enc_center(traj_center.view(-1, *traj_center.shape[-3:]))
-        traj_center_enc = traj_center_enc.view(*traj_center.shape[:2], -1)
+    def forward(self, traj_r, traj_l, traj_c, lang, traj_len, lang_len):
+        traj_r_enc = self.img_enc_r(traj_r.view(-1, *traj_r.shape[-3:]))
+        traj_r_enc = traj_r_enc.view(*traj_r.shape[:2], -1)
+        traj_l_enc = self.img_enc_l(traj_l.view(-1, *traj_l.shape[-3:]))
+        traj_l_enc = traj_l_enc.view(*traj_l.shape[:2], -1)
+        traj_c_enc = self.img_enc_c(traj_c.view(-1, *traj_c.shape[-3:]))
+        traj_c_enc = traj_c_enc.view(*traj_c.shape[:2], -1)
 
-        traj_enc = torch.cat([traj_right_enc, traj_left_enc, traj_center_enc], dim=-1)
+        traj_enc = torch.cat([traj_r_enc, traj_l_enc, traj_c_enc], dim=-1)
         _, traj_enc = lstm_helper(traj_enc, traj_len, self.traj_encoder)
 
         lang_emb = self.embedding(lang)
@@ -141,58 +146,61 @@ class Predict:
                 scores[start, end] = (prob_norm[0, 1] - prob_norm[0, 0])
         return scores
 
-    def predict_test(self, traj_right, traj_left, traj_center, lang, traj_len, lang_len):
+    def predict_test(self, traj_r, traj_l, traj_c, lang, traj_len, lang_len):
         self.model.train()
-        traj_right_sampled = traj_right
-        traj_right_sampled = np.array(traj_right_sampled)
-        traj_right_sampled = torch.from_numpy(traj_right_sampled)
-        traj_right_sampled = traj_right_sampled.cuda().float()
-        traj_right_sampled = torch.transpose(traj_right_sampled, 3, 4)
-        traj_right_sampled = torch.transpose(traj_right_sampled, 2, 3)
-        traj_left_sampled = traj_left
-        traj_left_sampled = np.array(traj_left_sampled)
-        traj_left_sampled = torch.from_numpy(traj_left_sampled)
-        traj_left_sampled = traj_left_sampled.cuda().float()
-        traj_left_sampled = torch.transpose(traj_left_sampled, 3, 4)
-        traj_left_sampled = torch.transpose(traj_left_sampled, 2, 3)
-        traj_center_sampled = traj_center
-        traj_center_sampled = np.array(traj_center_sampled)
-        traj_center_sampled = torch.from_numpy(traj_center_sampled)
-        traj_center_sampled = traj_center_sampled.cuda().float()
-        traj_center_sampled = torch.transpose(traj_center_sampled, 3, 4)
-        traj_center_sampled = torch.transpose(traj_center_sampled, 2, 3)
+        traj_r_sampled = traj_r
+        traj_r_sampled = np.array(traj_r_sampled)
+        traj_r_sampled = torch.from_numpy(traj_r_sampled)
+        traj_r_sampled = traj_r_sampled.cuda().float()
+        traj_r_sampled = torch.transpose(traj_r_sampled, 3, 4)
+        traj_r_sampled = torch.transpose(traj_r_sampled, 2, 3)
+        traj_l_sampled = traj_l
+        traj_l_sampled = np.array(traj_l_sampled)
+        traj_l_sampled = torch.from_numpy(traj_l_sampled)
+        traj_l_sampled = traj_l_sampled.cuda().float()
+        traj_l_sampled = torch.transpose(traj_l_sampled, 3, 4)
+        traj_l_sampled = torch.transpose(traj_l_sampled, 2, 3)
+        traj_c_sampled = traj_c
+        traj_c_sampled = np.array(traj_c_sampled)
+        traj_c_sampled = torch.from_numpy(traj_c_sampled)
+        traj_c_sampled = traj_c_sampled.cuda().float()
+        traj_c_sampled = torch.transpose(traj_c_sampled, 3, 4)
+        traj_c_sampled = torch.transpose(traj_c_sampled, 2, 3)
         lang = lang.cuda().long()
         traj_len = torch.Tensor(traj_len)
         lang_len = torch.Tensor(lang_len)
-        prob, lang_emb = self.model(traj_right_sampled, traj_left_sampled, traj_center_sampled, lang, traj_len, lang_len)
+        prob, lang_emb = self.model(traj_r_sampled, traj_l_sampled, traj_c_sampled, lang, traj_len, lang_len)
         return prob, lang_emb
 
-    def predict(self, traj_right, traj_left, traj_center, lang):
+    def predict(self, traj_r, traj_l, traj_c, lang):
         self.model.eval()
         with torch.no_grad():
-            traj_right_sampled = traj_right[::-1][::10][::-1]
-            traj_right_sampled = np.array(traj_right_sampled)
-            traj_right_sampled = torch.from_numpy(traj_right_sampled)
-            traj_right_sampled = traj_right_sampled.cuda().float()
-            traj_right_sampled = torch.transpose(traj_right_sampled, 2, 3)
-            traj_right_sampled = torch.transpose(traj_right_sampled, 1, 2)
-            traj_left_sampled = traj_left[::-1][::10][::-1]
-            traj_left_sampled = np.array(traj_left_sampled)
-            traj_left_sampled = torch.from_numpy(traj_left_sampled)
-            traj_left_sampled = traj_left_sampled.cuda().float()
-            traj_left_sampled = torch.transpose(traj_left_sampled, 2, 3)
-            traj_left_sampled = torch.transpose(traj_left_sampled, 1, 2)
-            traj_center_sampled = traj_center[::-1][::10][::-1]
-            traj_center_sampled = np.array(traj_center_sampled)
-            traj_center_sampled = torch.from_numpy(traj_center_sampled)
-            traj_center_sampled = traj_center_sampled.cuda().float()
-            traj_center_sampled = torch.transpose(traj_center_sampled, 2, 3)
-            traj_center_sampled = torch.transpose(traj_center_sampled, 1, 2)
+            traj_r_sampled = traj_r[::-1][::10][::-1]
+            traj_r_sampled = np.array(traj_r_sampled)
+            traj_r_sampled = torch.from_numpy(traj_r_sampled)
+            traj_r_sampled = traj_r_sampled.cuda().float()
+            traj_r_sampled = torch.transpose(traj_r_sampled, 2, 3)
+            traj_r_sampled = torch.transpose(traj_r_sampled, 1, 2)
+            traj_l_sampled = traj_l[::-1][::10][::-1]
+            traj_l_sampled = np.array(traj_l_sampled)
+            traj_l_sampled = torch.from_numpy(traj_l_sampled)
+            traj_l_sampled = traj_l_sampled.cuda().float()
+            traj_l_sampled = torch.transpose(traj_l_sampled, 2, 3)
+            traj_l_sampled = torch.transpose(traj_l_sampled, 1, 2)
+            traj_c_sampled = traj_c[::-1][::10][::-1]
+            traj_c_sampled = np.array(traj_c_sampled)
+            traj_c_sampled = torch.from_numpy(traj_c_sampled)
+            traj_c_sampled = traj_c_sampled.cuda().float()
+            traj_c_sampled = torch.transpose(traj_c_sampled, 2, 3)
+            traj_c_sampled = torch.transpose(traj_c_sampled, 1, 2)
             lang = lang.cuda().long()
-            traj_len = torch.Tensor([len(traj_right_sampled)])
+            traj_len = torch.Tensor([len(traj_r_sampled)])
             lang_len = torch.Tensor([len(lang)])
-            prob = self.model(torch.unsqueeze(traj_right_sampled, 0) / 255., torch.unsqueeze(traj_left_sampled, 0) / 255., \
-                torch.unsqueeze(traj_center_sampled, 0) / 255., torch.unsqueeze(lang, 0), traj_len, lang_len)
+            prob = self.model(
+                torch.unsqueeze(traj_r_sampled, 0) / 255., 
+                torch.unsqueeze(traj_l_sampled, 0) / 255.,
+                torch.unsqueeze(traj_c_sampled, 0) / 255., 
+                torch.unsqueeze(lang, 0), traj_len, lang_len)
         return prob
 
     def update(self, traj_r, traj_l, traj_c, lang, label):
@@ -236,28 +244,25 @@ class Train:
         self.model = Model(args).cuda()
         self.train_data_loader = train_data_loader
         self.valid_data_loader = valid_data_loader
-        params_img_enc = list(self.model.img_enc_right.parameters()) + list(self.model.img_enc_left.parameters()) + list(self.model.img_enc_center.parameters())
-        params_lang_enc = list(self.model.embedding.parameters()) + list(self.model.descr_encoder.parameters())
-        params_rest = list(filter(lambda kv: 'img_enc' not in kv[0] and 'embedding' not in kv[0] and 'descr_enc' not in kv[0], self.model.named_parameters()))
-        params_rest = list(map(lambda x: x[1], params_rest))
         self.optimizer = optim.Adam(
             self.model.parameters(),
             lr=self.args.lr)
 
-    def run_batch(self, traj_right, traj_left, traj_center, lang, traj_len, lang_len, labels, weights, is_train):
+    def run_batch(self, traj_r, traj_l, traj_c, lang, traj_len, 
+        lang_len, labels, weights, is_train):
         if is_train:
             self.model.train()
             self.optimizer.zero_grad()
         else:
             self.model.eval()
 
-        traj_right = traj_right.cuda().float()
-        traj_left = traj_left.cuda().float()
-        traj_center = traj_center.cuda().float()
+        traj_r = traj_r.cuda().float()
+        traj_l = traj_l.cuda().float()
+        traj_c = traj_c.cuda().float()
         lang = lang.cuda().long()
         labels = torch.Tensor(labels).cuda().long()
         weights = weights.cuda().float()
-        pred, _ = self.model(traj_right, traj_left, traj_center, lang, traj_len, lang_len)
+        pred, _ = self.model(traj_r, traj_l, traj_c, lang, traj_len, lang_len)
         prob = torch.tanh(pred[:, 0])
         loss = torch.nn.MSELoss()(prob, weights*labels)
         pred = prob
@@ -272,8 +277,11 @@ class Train:
         pred_all = []
         labels_all = []
         loss_all = []
-        for frames_right, frames_left, frames_center, descr, descr_enc, traj_len, descr_len, labels, _, _, weights in data_loader:
-            pred, loss = self.run_batch(frames_right, frames_left, frames_center, descr_enc, traj_len, descr_len, labels, weights, is_train)
+        for frames_r, frames_l, frames_c, descr, descr_enc, \
+            traj_len, descr_len, labels, _, _, weights in data_loader:
+            pred, loss = self.run_batch(
+                frames_r, frames_l, frames_c, descr_enc, traj_len, 
+                descr_len, labels, weights, is_train)
             pred_all += pred.tolist()
             labels_all += (weights * labels).tolist()
             loss_all.append(loss)
@@ -284,8 +292,10 @@ class Train:
         best_val_acc = 0.
         epoch = 1
         while True:
-            valid_loss, valid_acc, valid_cm = self.run_epoch(self.valid_data_loader, is_train=False)
-            train_loss, train_acc, train_cm = self.run_epoch(self.train_data_loader, is_train=True)
+            valid_loss, valid_acc, valid_cm = self.run_epoch(
+                self.valid_data_loader, is_train=False)
+            train_loss, train_acc, train_cm = self.run_epoch(
+                self.train_data_loader, is_train=True)
             print('Epoch: {}\tTL: {:.2f}\tTA: {:.2f}\tVL: {:.2f}\tVA: {:.2f}'.format(
                 epoch, train_loss, 100. * train_acc, valid_loss, 100. * valid_acc))
             if valid_acc > best_val_acc:
